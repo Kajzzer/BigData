1faccdb82be2adc8a8a30fbdb05c8221361137c3
Dimitri Hooftman <hooftman.d@gmail.com>
2022-03-18 17:41:36 +0100

    Part of speech tagging added for Review class

--- ReviewClass.py
+++ ReviewClass.py
@@ -1,6 +1,13 @@
 from data_cleaning import word_tokenizer
 
+import spacy
+
 class Review():
+
+    fr_core_news_sm = spacy.load("de_core_news_sm")
+    de_core_news_sm = spacy.load("de_core_news_sm")
+    en_core_web_sm = spacy.load("en_core_web_sm")
+
     def __init__(
         self, review_id, product_id, product_parent, product_title, vine, 
         verified_purchase, review_headline, review_body, review_date,
@@ -18,9 +25,24 @@ class Review():
         self.marketplace_id = marketplace_id
         self.product_category_id = product_category_id
         self.label = label
+        
+    def SelectLanguageModel(self):
+        if self.marketplace_id == 2:
+            self.model = Review.fr_core_news_sm
+        elif self.marketplace_id == 3:
+            self.model = Review.de_core_news_sm
+        else:
+            self.model = Review.en_core_web_sm
 
     def ReviewBodyCharCount(self):
         return len(self.review_body)
 
     def ReviewBodyWordCount(self):
         return len(word_tokenizer(self.review_body))
+
+    def TaggedReviewBody(self):
+        if not hasattr(self, 'model'):
+            self.SelectLanguageModel()
+
+        sentence = self.model(self.review_body)
+        return [{'text': word.text, 'pos': word.pos_, 'tag': word.tag_} for word in sentence]
