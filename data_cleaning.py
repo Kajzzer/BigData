33dc1ad88a2418bd04ff5ce56028012192e0aadf
Kaj Meijer <k.d.meijer17@gmail.com>
2022-03-23 13:42:01 +0100

    Added data cleaning methods.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,7 @@
 # import nltk
 # nltk.download('wordnet')
 from nltk.tokenize import wordpunct_tokenize, RegexpTokenizer, sent_tokenize
-from nltk.stem import PorterStemmer, WordNetLemmatizer 
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 
 
@@ -44,3 +44,57 @@ def lemmatizer(tokenized_review):
     Lemmatize the tokenized  review
     """
     return [WordNetLemmatizer().lemmatize(word) for word in tokenized_review]
+
+def fix_title_headline(dataframe, dataframe_row):
+    """
+    Fix the product title and review headline if they have been mixed up
+    """
+    # get the possible titles of the product id of this row
+    rows = dataframe[dataframe['product_id'] == dataframe_row['product_id']]
+
+    # the unique titles of this product
+    unique_titles = rows['product_title'].unique()
+
+    # get the review headline
+    review_headline = dataframe_row['review_headline']
+
+    # check if the review_headline is in the unique titles
+    if review_headline in unique_titles:
+
+        # replace the review headline of the row with the title
+        dataframe_row['review_headline'] = dataframe_row['product_title']
+
+        # replace the title of the row with the review headline
+        dataframe_row['product_title'] = review_headline
+
+    # return the modified row
+    return dataframe_row
+
+def remove_accent_spam(text, threshold=0.075):
+    """
+    Remove the accents if they have been spammed in the text.
+    If there is an accent spam in the text, that text only includes
+    the vowels with acute accents instead of the i.
+    """
+    # the total number of characters
+    total = len(text)
+
+    # the number of accents
+    acutes = 0
+
+    # the map of acute char with their non accented variant
+    acute_map = np.array([['á', 'a'], ['Á', 'A'], ['é', 'e'],
+                          ['É', 'E'], ['ớ', 'o'], ['ó', 'o'],
+                          ['Ó', 'O'], ['ú', 'u'], ['Ú', 'U']])
+
+    # iterate over the characters and check if acute is in their unicode name
+    for char in text:
+        if char in acute_map[:, 1]:
+            return text
+
+    # clean the text
+    for [accent, char] in acute_map:
+        text = re.sub(accent, char, text)
+
+    # return the cleaned text
+    return text
